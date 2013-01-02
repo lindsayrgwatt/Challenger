649047125da4bd1a963de266c8763910c2a7ab8a
imackinn <imackinn@alumni.uwaterloo.ca>
2013-01-01 19:19:00 -0800

    More basic structure

---
+++ app/controllers/map.js
@@ -0,0 +1,10 @@
+function doClick(evt){
+    Ti.API.info("Annotation " + evt.title + " clicked, id: " + evt.annotation.myid);
+
+    // Check for all of the possible names that clicksouce
+    // can report for the left button/view.
+    if (evt.clicksource == 'leftButton' || evt.clicksource == 'leftPane' ||
+        evt.clicksource == 'leftView') {
+        Ti.API.info("Annotation " + evt.title + ", left button clicked.");
+    }
+};
