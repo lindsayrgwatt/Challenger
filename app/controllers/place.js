18507b19010c493c0024fea9c67b0802f04680c4
imackinn <imackinn@alumni.uwaterloo.ca>
2013-01-02 18:06:28 -0800

    add the basic theme to the project

--- app/controllers/place.js
+++ app/controllers/place.js
@@ -17,12 +17,14 @@ exports.setPlace = function( placemark ){
 	
 	$.placeName.text = placemark.place.name;
 	
-	$.mapImage.image = placemark.place.map_url;
+	$.mapImage.image = placemark.place.map_url.replace("size=100x100", "size="+$.mapImage.width+"x"+$.mapImage.height);
 	
 	$.streetAddress.text = placemark.place.street_address;
 	$.placeTags.text = placemark.tags.join(",")
 	$.placeMemo.text = placemark.memo;
 	
 	$.imagesViewHolder.height = 50;
+	
+	
 	
 }
